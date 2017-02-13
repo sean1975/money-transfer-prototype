7a49c83e50a021cca15d0352b0e098fc8afe5a4e
sean1975 <sean1975@gmail.com>
2017-02-13 21:53:06 +1000

    change request method from GET to POST; use webapp2 python framework as controller; use JINJA2 for rendering html pages

--- main.py
+++ main.py
@@ -1,11 +1,45 @@
 import webapp2
+import os
+import jinja2
+
+'''
+pages = [
+         'index.html',
+         'personal_information.html',
+         'recipient.html',
+         'photocopy.html',
+         'consents.html',
+         'consents2.html',
+         'agreement.html',
+         'confirm.html',
+         'complete.html'
+         ]
+'''
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 class MainPage(webapp2.RequestHandler):
+    """Controller for handling user requests"""
+    
+    def render(self, page):
+        template = JINJA_ENVIRONMENT.get_template(page)
+        return template.render(self.request.params)
+    
+    def post(self):
+        return self.get()
+    
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        page = 'html/index.html'
+        if len(self.request.path) > 1:
+            page = 'html' + self.request.path
+        #param = self.request.params
+        self.response.write(self.render(page))
 
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/[^/]*\.html', MainPage),
 ], debug=True)
